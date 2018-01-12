ce80702d152def97d1e0c7d07ff737706f1b9f17
Pavlo GRITSENKO <pgritsen@e2r13p8.unit.ua>
2018-01-12 13:51:08 +0200

    ft_strsplit critical memmory leak fixed!

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,13 +6,13 @@
 /*   By: pgritsen <pgritsen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 20:24:49 by pgritsen          #+#    #+#             */
-/*   Updated: 2017/12/13 15:42:28 by pgritsen         ###   ########.fr       */
+/*   Updated: 2018/01/12 13:50:26 by pgritsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	s_count(char const *s, char c)
+static size_t	s_count(char *s, char c)
 {
 	size_t	words;
 
@@ -21,12 +21,9 @@ static size_t	s_count(char const *s, char c)
 	words = 1;
 	while (*s)
 	{
-		if (*s == c)
-		{
-			words++;
+		if (*s == c && words++)
 			while (*s && *s == c)
 				s++;
-		}
 		while (*s && *s != c)
 			s++;
 	}
@@ -37,12 +34,29 @@ static void		*clean_list(char **list)
 {
 	char	**tmp;
 
+	if (!list)
+		return (NULL);
 	tmp = list;
-	while (tmp)
+	while (*tmp)
 		free(*tmp++);
-	if (list)
-		free(list);
+	free(list);
 	return (NULL);
+}
+
+static int		add_vord(char **list, size_t *it, char **p_s, char c)
+{
+	char	*trash;
+	size_t	w_l;
+
+	w_l = ft_strclen(*p_s, c);
+	if (!(list[*it] = (char *)malloc(sizeof(char) * (w_l + 1))))
+		return (0);
+	*list[*it] = 0;
+	trash = *p_s;
+	ft_strlcat(list[(*it)++], (*p_s += w_l) - w_l, w_l + 1);
+	*p_s = ft_strntrim(*p_s, c);
+	free(trash);
+	return (1); 
 }
 
 char			**ft_strsplit(char const *s, char c)
@@ -51,25 +65,18 @@ char			**ft_strsplit(char const *s, char c)
 	char	*p_s;
 	long	s_c;
 	size_t	it;
-	size_t	w_l;
 
 	if (!s)
 		return (NULL);
-	s_c = (long)s_count(ft_strntrim(s, c), c);
+	s_c = (long)s_count((p_s = ft_strntrim(s, c)), c);
 	if (!(list = (char **)malloc(sizeof(char *) * (s_c + 1))))
 		return (NULL);
-	if (!*(p_s = ft_strntrim(s, c)))
-		*list = (char *)--s_c;
+	!*p_s ? *list = (char *)--s_c : 0;
 	it = 0;
 	while (s_c-- > 0)
-	{
-		p_s = ft_strntrim(p_s, c);
-		w_l = ft_strclen(p_s, c);
-		if (!(list[it] = (char *)malloc(sizeof(char) * (w_l + 1))))
+		if (!add_vord(list, &it, &p_s, c))
 			return (clean_list(list));
-		*list[it] = 0;
-		ft_strlcat(list[it++], (p_s += w_l) - w_l, w_l + 1);
-	}
+	free(p_s);
 	list[it] = 0;
 	return (list);
 }
